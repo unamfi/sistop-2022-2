d063cc8bcd9ac9155f01283920ffd6efd3981c36
Gunnar Wolf <gwolf@gwolf.org>
2022-02-10 19:42:56 -0600

    Ejemplos utilizados para explicar interrupciones / excepciones / llamadas al sistema

---
+++ ejemplos_en_clase/1.Introducción/entero_y_flotante.c
@@ -0,0 +1,10 @@
+void main() {
+    int a, b, c;
+    float d, e, f;
+    a = 1;
+    b = 2;
+    c = a + b;
+    d = 1.0;
+    e = 2.0;
+    f = d + e;
+}
---
+++ ejemplos_en_clase/1.Introducción/simple_printf.s
@@ -0,0 +1,28 @@
+	.file	"simple_printf.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"\302\241Hola mundo!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 11.2.0-14) 11.2.0"
+	.section	.note.GNU-stack,"",@progbits
