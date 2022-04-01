c18facd1506afb31305f76b9493c7d5047d2b029
André Rosales <andre.rosales@comunidad.unam.mx>
2022-04-01 02:42:59 -0500

    Agregando impresiones en pantalla para detallar lo que sucede en cada paso

--- proyectos/2/AbadAldo-RosalesAndré/proyecto2.py
+++ proyectos/2/AbadAldo-RosalesAndré/proyecto2.py
@@ -10,12 +10,14 @@ listoParaIrse = threading.Semaphore(0)
 mutex = threading.Semaphore(1)
 
 def comensal(num):
-    
+   
+    global comiendo, haciendoSobremesa
     obtenerComida(num)
     
     mutex.acquire()
     comiendo+=1
     
+    print("Comensal #%d verificando si se puede sentar..." % num)
     if comiendo == 2 and haciendoSobremesa == 1:
         listoParaIrse.release()
         haciendoSobremesa -= 1
@@ -28,15 +30,19 @@ def comensal(num):
     haciendoSobremesa += 1
 
     if comiendo == 1 and haciendoSobremesa == 1:
+       
+        print("Comensal #%d esperando a que otro comensal termine de comer..." % num)
         mutex.release()
-        listoParaIrse.release()
+        listoParaIrse.acquire()
 
     elif comiendo == 0 and haciendoSobremesa == 2:
+        print("Comensal #%d notificando que ha terminado al comensal que lo esté esperando..." % num)
         listoParaIrse.release()
         haciendoSobremesa -= 2
         mutex.release()
 
     else:
+        print("Comensal #%d se ha quedado solo... mejor irse" % num)
         haciendoSobremesa -= 1
         mutex.release()
 
@@ -47,12 +53,12 @@ def obtenerComida(num):
     time.sleep(random.random() / 100)
 
 def comer(num):
-    print("Comensal #%d comiendo..." % num)
+    print("Comensal #%d sentandose a comer..." % num)
     time.sleep(random.random() / 100)
 
 def retirarse(num):
     print("Comensal #%d retirándose..." %num)
     time.sleep(random.random() / 100)
 
-for i in range(6):
+for i in range(4):
     threading.Thread(target=comensal, args=[i]).start()
