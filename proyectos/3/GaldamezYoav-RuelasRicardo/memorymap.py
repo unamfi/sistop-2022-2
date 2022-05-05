f0b895784cbb23c19d39c3f462d91ba8303032de
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2022-05-05 14:25:56 -0500

    Agregado encabezado para la salida y forma de separar los datos

--- proyectos/3/GaldamezYoav-RuelasRicardo/memorymap.py
+++ proyectos/3/GaldamezYoav-RuelasRicardo/memorymap.py
@@ -10,23 +10,38 @@ def lectura_archivo(pid):
 
     except:
         print("Algo salio mal!")
-        print("Error: ", sys.exc_info()[0])
+        print("\nError: ", sys.exc_info()[0])
 
         return "ERROR"
-        
+
+#def obtener_datos(pid)
+
 #Proceso principal del programa (permite realizar varias consultas)
 while(1):
-    pid = input("Ingresa el PID ('s' para salir): ")
+    pid = input("\nIngresa el PID ('s' para salir): ")
+    
     #Salida del programa
-    if pid == "s":
-        print("Hasta la proxima!")
+    if pid.lower() == "s":
+        print("\nHasta la proxima!")
         break
 
+    #Asignamos a una variable el contenido obtenido del archivo
     contenido = lectura_archivo(pid)
 
+    #Verificamos si la lectura del archivo nos dio error para comenzar de nuevo,
+    #de no ser así procedemos con el programa
     if contenido != "ERROR":
+
+
+        print("║"+'Uso'.center(18),end='')
+        print("║"+'Inicio-Fin'.center((13*2)+1),end='')
+        print("║"+'Tamaño'.center(12),end='')
+        print("║"+'Num. págs.'.center(10),end='')
+        print("║"+'Perm.'.center(5),end='')
+        print("║"+' Uso/Mapeo')
+
         for linea in contenido:
             print(linea)
-
-
-
+            #Separamos los datos de cada linea en una lista
+            #Esta separación se hara por espacios en blanco
+            datosSeparados = linea.split()
