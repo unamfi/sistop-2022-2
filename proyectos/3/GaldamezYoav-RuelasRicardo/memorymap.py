b9cc306484a86069f2d2d0b9b3c4a2312801c203
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2022-05-05 13:34:02 -0500

    Mejora de la estructura del programa, loop principal y creacion de funcion 'lectura_archivo'

--- proyectos/3/GaldamezYoav-RuelasRicardo/memorymap.py
+++ proyectos/3/GaldamezYoav-RuelasRicardo/memorymap.py
@@ -1,17 +1,32 @@
 import sys
 
-#Obtenemos el archivo con el contenido de la memoria de /proc/$pid/maps
-pid = input("Ingresa el PID: ")
+#Obtenemos el archivo con el contenido de la memoria a partir de /proc/$pid/maps
+def lectura_archivo(pid):
+    try:
+        direccionArch = '/proc/'+pid+'/maps'
+        archivo = open(direccionArch, 'r')
 
-try:
-    direccionArch = '/proc/'+pid+'/maps'
-    archivo = open(direccionArch, 'r')
+        return archivo.readlines()
 
-    contenido = archivo.readlines()
+    except:
+        print("Algo salio mal!")
+        print("Error: ", sys.exc_info()[0])
 
-    for linea in contenido:
-        print(linea)
+        return "ERROR"
+        
+#Proceso principal del programa (permite realizar varias consultas)
+while(1):
+    pid = input("Ingresa el PID ('s' para salir): ")
+    #Salida del programa
+    if pid == "s":
+        print("Hasta la proxima!")
+        break
 
-except:
-    print("Algo salio mal!")
-    print("Error: ", sys.exc_info()[0])
+    contenido = lectura_archivo(pid)
+
+    if contenido != "ERROR":
+        for linea in contenido:
+            print(linea)
+
+
+
