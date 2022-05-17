8017e78da4f58cad4f3c238cea2dd6d6112e2781
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2022-05-17 17:30:44 -0500

    Definición de clase para superbloque y método para su creación. Estructura mejorada para la conexión con el sistema de archivos.

--- proyectos/4/GaldamezYoav-RuelasRicardo/fiufs_controller.py
+++ proyectos/4/GaldamezYoav-RuelasRicardo/fiufs_controller.py
@@ -1,7 +1,76 @@
-archivo = open('fiunamfs.img')
+#Se inicia la conexión (lectura de la imagen) con el sistema de archivos
+try:
+	sistema = open('fiunamfs.img', encoding='ASCII') #Importante especificar y trabajar con codififación ASCII
+#Si el archivo no se encuentra, se le indica al usuario
+except FileNotFoundError:
+	print("\n¡Algo salio mal!\n")
+	print("Error: El sistema de archivos no fue encontrado.")
+	print("Por favor, comprueba que el programa y el archivo"
+		+ "se encuentran en el mismo directorio e intenta de nuevo.")
+#Si el usuario no cuenta con permisos, se le indica
+except PermissionError:
+	print("\n¡Algo salio mal!\n")
+	print("Error: El sistema de archivos no fue encontrado.")
+	
 
-archivo.seek(2048)
+#Clase SuperBloque que mantendrá seguros y globales los datos del primer cluster
+class SuperBloque:
+	
+	def __init__(self,
+				nombre,
+				version,
+				et_volumen,
+				tam_cluster,
+				num_clusters_dir,
+				num_clusters_uni):
 
-lectura = archivo.read(2048*4)
+		self.nombre = nombre
+		self.version = version
+		self.et_volumen = et_volumen
+		self.tam_cluster = tam_cluster #Tamaño en bytes
+		self.num_clusters_dir = num_clusters_dir
+		self.num_clusters_uni = num_clusters_uni
 
-print(lectura)
+
+#Método que permite leer los datos del superbloque y asignarlos a un objeto de este tipo
+def crear_super_bloque():
+	#En este método ocuparemos las funciones seek y read para archivos
+	#Seek se encarga de establecer un cursor en la posición dada por parametro
+	#Read se encarga de leer la cantidad de datos especificados como parametro
+
+	#Leemos el nombre del sistema de archivos en las posiciones 0-8 de la imagen
+	sistema.seek(0)
+	nombre = sistema.read(8)
+
+	#Leemos la versión de implementación en las posiciones 10-13
+	sistema.seek(10)
+	version = sistema.read(4)
+
+	#Leemos la etiqueta del volumen en las posiciones 20-35
+	sistema.seek(20)
+	et_volumen = sistema.read(16)
+
+	#Leemos el tamaño del cluster (en bytes) en las posiciones 40-45
+	sistema.seek(40)
+	tam_cluster = sistema.read(6)
+
+	#Leemos el número de clusters que mide el directorio en las posiciones 47-49
+	sistema.seek(47)
+	num_clusters_dir = sistema.read(3)
+
+	#Leemos el número de clusters que mide la unidad completa en las posiciones 52-60
+	sistema.seek(52)
+	num_clusters_uni = sistema.read(9)
+
+	
+
+	return SuperBloque(nombre,
+					version,
+					et_volumen,
+					tam_cluster,
+					num_clusters_dir,
+					num_clusters_uni)
+
+info_sistema = crear_super_bloque()
+
+print(info_sistema.nombre)
